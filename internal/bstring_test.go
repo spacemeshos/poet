e7cad7203c8d4526eb8893000a20040b3f84078d
Aviv Eyal <aviveyal07@gmail.com>
2018-10-20 00:21:20 +0300

    Additional tests

--- internal/bstring_test.go
+++ internal/bstring_test.go
@@ -22,9 +22,22 @@ func TestRandomBinaryString(t *testing.T) {
 		const d = uint(63)
 		b, err := f.NewRandomBinaryString(d)
 		assert.NoError(t, err)
-		println(b.GetStringValue())
 		assert.Equal(t, uint(d), b.GetDigitsCount())
 	}
+}
+
+func TestInvalidBinaryString(t *testing.T) {
+	f := NewSMBinaryStringFactory()
+	// try to create a binary string from an invalid string - contains chars other than 0 and 1
+	_, err := f.NewBinaryString("0102")
+	assert.Error(t, err)
+}
+
+func TestInvalidBinaryStringFromInt(t *testing.T) {
+	f := NewSMBinaryStringFactory()
+	// try to create from int with insufficient number of digits to encode int value to bits
+	_, err := f.NewBinaryStringFromInt(1025, 5)
+	assert.Error(t, err)
 }
 
 func TestBinaryStringFromInt(t *testing.T) {
@@ -35,21 +48,21 @@ func TestBinaryStringFromInt(t *testing.T) {
 
 	b, err := f.NewBinaryStringFromInt(v, 7)
 	assert.NoError(t, err)
-	println(b.GetStringValue())
+	//println(b.GetStringValue())
 	assert.Equal(t, uint(7), b.GetDigitsCount())
 	assert.Equal(t, v, b.GetValue())
 
 	// encode to an 8 bits binary string
 	b, err = f.NewBinaryStringFromInt(v, 8)
 	assert.NoError(t, err)
-	println(b.GetStringValue())
+	//println(b.GetStringValue())
 	assert.Equal(t, uint(8), b.GetDigitsCount())
 	assert.Equal(t, v, b.GetValue())
 
 	// encode to an a 63 bits binary string
 	b, err = f.NewBinaryStringFromInt(v, 63)
 	assert.NoError(t, err)
-	println(b.GetStringValue())
+	//println(b.GetStringValue())
 	assert.Equal(t, uint(63), b.GetDigitsCount())
 	assert.Equal(t, v, b.GetValue())
 }
