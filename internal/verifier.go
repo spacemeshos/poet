7407948b0ef50743b407cce998beed9c51e829cc
Aviv Eyal <aviveyal07@gmail.com>
2018-10-20 00:53:06 +0300

    CreteNipChallenge

--- internal/verifier.go
+++ internal/verifier.go
@@ -1,6 +1,8 @@
 package internal
 
 import (
+	"bytes"
+	"encoding/binary"
 	"github.com/spacemeshos/poet-ref/shared"
 	"math/big"
 )
@@ -12,16 +14,16 @@ type Identifier = shared.Identifier
 type HashFunc = shared.HashFunc
 
 type SMVerifier struct {
-	x []byte
-	n uint
-	h HashFunc
+	x []byte	// commitment
+	n uint		// n param 1 <= n <= 63
+	h HashFunc	// Hx()
 }
 
 func (s *SMVerifier) Verify(c Challenge, p Proof) bool {
 	return true
 }
 
-func (s *SMVerifier) CreteNipChallenge(phi []byte) Challenge {
+func (s *SMVerifier) CreteNipChallenge(phi []byte) (Challenge,error) {
 
 	// γ := (Hx(φ,1),...Hx(φ,t))
 
@@ -29,18 +31,31 @@ func (s *SMVerifier) CreteNipChallenge(phi []byte) Challenge {
 
 	f := NewSMBinaryStringFactory()
 
-	for i := 0; i < shared.T; i++ {
-		// set identifier
-		b := s.h.Hash(phi) // todo: add i in bytes (bigEndian)
+	buf := new(bytes.Buffer)
 
+	for i := 0; i < shared.T; i++ {
+
+		buf.Reset()
+		err := binary.Write(buf, binary.BigEndian, i)
+		if err != nil {
+			// return error here
+			return Challenge{}, err
+		}
+
+		// pack (phi,t) into a bytes array
+		d := append(phi, buf.Bytes()...)
+
+		// Compute Hx(phi,t)
+		b := s.h.Hash(d)
+
+		// Convert hash to Uint64 and create a W bits long string
 		bg := new(big.Int).SetBytes(b[:])
 		v := bg.Uint64()
-
 		s, _ := f.NewBinaryStringFromInt(v, shared.W)
 		data[i] = Identifier(s.GetStringValue())
 	}
 
-	return Challenge{Data: data}
+	return Challenge{Data: data}, nil
 }
 
 // create a random challenge that can be used to challenge a prover
