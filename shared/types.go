68bb9ea13e6fce15b8eb28c10219f425e3876241
Aviv Eyal <aviveyal07@gmail.com>
2018-10-20 16:14:26 +0300

    Comment shared types

--- shared/types.go
+++ shared/types.go
@@ -2,34 +2,35 @@ package shared
 
 const (
 	T  = 150 // security param
-	W  = 256 // bits
-	WB = 32  // w in bytes
+	W  = 256 // bits - label size and Hx() output size
+	WB = 32  // W in bytes
 )
 
 type HashFunc interface {
 	Hash(data []byte) [WB]byte
 }
 
-type Label [WB]byte    // label is WB bytes
+type Label [WB]byte    // label is WB bytes long
 type Labels []Label    // an ordered list of Labels
-type Identifier string // variable length binary string
-
-type Proof struct {
-	Phi Label     // dag root label
-	L   [T]Labels // a list of T lists of labels
-}
+type Identifier string // variable-length binary string. e.g. "0011010" Only 0s and 1s are allows chars. Identifiers are n bits long.
 
 type Challenge struct {
-	Data [T]Identifier
+	Data [T]Identifier // A list of T identifiers
+}
+
+type Proof struct {
+	Phi Label     // dag root label value
+	L   [T]Labels // T lists of labels - one for every of the T challenges
 }
 
 type IBasicVerifier interface {
 
-	// Verify proof p for challenge c with verifier initialized with x and n
+	// Verify proof p provided for challenge c using a verifier initialized with x and n where T and W shared between verifier and prover
 	Verify(c Challenge, p Proof) bool
 
-	// Create a NIP challenge based on phi (root label value)
+	// Create a NIP challenge based on Phi (root label value provided by a proof)
 	CreteNipChallenge(phi []byte) (Challenge, error)
 
+	// create a random challenge, that consists of T random identifies (each n bits long)
 	CreteRndChallenge() (Challenge, error)
 }
