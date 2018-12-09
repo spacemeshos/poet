5ae377bfce350154ac0d5162bd9a2e2bd62c3ab9
Aviv Eyal <aviveyal07@gmail.com>
2018-12-09 10:19:57 +0200

    Add support for multiple iterations per Hx() - unused

--- shared/sha256_hash.go
+++ shared/sha256_hash.go
@@ -9,11 +9,17 @@ import (
 type sha256Hash struct {
 	x    []byte // arbitrary binary data
 	hash hash.Hash
+	iters int
+	emptySlice []byte
 }
 
 // Returns a new HashFunc Hx() for commitment X
 func NewHashFunc(x []byte) HashFunc {
-	return &sha256Hash{x: x, hash: sha256.New()}
+
+	// todo: pick it form parsm
+	iters := 50
+
+	return &sha256Hash{x: x, hash: sha256.New(), iters: iters}
 }
 
 // Hash implements Hx()
@@ -31,6 +37,33 @@ func (h *sha256Hash) Hash(data ...[]byte) []byte {
 
 	return h.hash.Sum([]byte{})
 }
+
+// Multiple iterations hash using client provided iters
+func (h *sha256Hash) HashIters(data ...[]byte) []byte {
+
+	h.hash.Reset()
+
+	// first, hash x
+	h.hash.Write(h.x)
+
+	// hash all user provided data
+	for _, d := range data {
+		_, _ = h.hash.Write(d)
+	}
+
+	digest := h.hash.Sum([]byte{})
+
+	// perform iter hashes of x and user data
+	for i := 0; i < h.iters; i++ {
+		h.hash.Reset()
+		h.hash.Write(h.x)
+		h.hash.Write(digest)
+		digest = h.hash.Sum(h.emptySlice)
+	}
+
+	return digest
+}
+
 
 func (h *sha256Hash) HashSingle(data []byte) []byte {
 	h.hash.Reset()
