fabb96d79ca5648ebea005a7da8ab32f756677ec
Moshe Shababo <dicentralized@gmail.com>
2019-03-12 11:26:35 +0200

    Basic service impl

--- service/round.go
+++ service/round.go
@@ -6,25 +6,30 @@ import (
 	"github.com/spacemeshos/merkle-tree"
 	"github.com/spacemeshos/poet-ref/internal"
 	"github.com/spacemeshos/poet-ref/shared"
+	"time"
 )
 
 type round struct {
-	cfg *Config
-	id  uint
+	cfg          *Config
+	id           int
+	opened       time.Time
+	executeStart time.Time
+	executeEnd   time.Time
 
 	commitments [][]byte
-	tree        *merkle.Tree
-	treeRoot    []byte
-	phi         []byte
+	merkleTree  *merkle.Tree
+	merkleRoot  []byte
+	nip         *shared.Proof
 
 	closedChan   chan struct{}
 	executedChan chan struct{}
 }
 
-func newRound(cfg *Config, id uint) *round {
+func newRound(cfg *Config, id int) *round {
 	r := new(round)
 	r.cfg = cfg
 	r.id = id
+	r.opened = time.Now()
 	r.closedChan = make(chan struct{})
 	r.executedChan = make(chan struct{})
 
@@ -38,42 +43,49 @@ func (r *round) submitCommitment(c []byte) error {
 }
 
 func (r *round) close() error {
-	r.tree = merkle.NewTree(merkle.GetSha256Parent)
+	r.merkleTree = merkle.NewTree(merkle.GetSha256Parent)
 	for _, c := range r.commitments {
-		err := r.tree.AddLeaf(c)
+		err := r.merkleTree.AddLeaf(c)
 		if err != nil {
 			return err
 		}
 	}
 
-	root, err := r.tree.Root()
+	root, err := r.merkleTree.Root()
 	if err != nil {
 		return nil
 	}
-	r.treeRoot = root
+	r.merkleRoot = root
 
 	close(r.closedChan)
 	return nil
 }
 
-func (r *round) execute() (shared.Label, error) {
-	prover, err := internal.NewProver(r.treeRoot, r.cfg.N, shared.NewHashFunc(r.treeRoot))
+func (r *round) execute() error {
+	// TODO(moshababo): use the config hash function
+	prover, err := internal.NewProver(r.merkleRoot, r.cfg.N, shared.NewHashFunc(r.merkleRoot))
 	if err != nil {
-		return shared.Label{}, err
+		return err
 	}
 
-	phi, err := prover.ComputeDag()
+	r.executeStart = time.Now()
+	_, err = prover.ComputeDag()
 	if err != nil {
-		return shared.Label{}, err
+		return err
 	}
-	r.phi = phi
+	nip, err := prover.GetNonInteractiveProof()
+	if err != nil {
+		return err
+	}
 
+	r.executeEnd = time.Now()
+	r.nip = &nip
 	close(r.executedChan)
-	return phi, nil
+	return nil
 }
 
 func (r *round) membershipProof(c []byte) ([][]byte, error) {
-	// temporary inefficient implementation
+	// TODO(moshababo): change this temp inefficient implementation
 	var ci uint64
 	for i, commitment := range r.commitments {
 		if bytes.Equal(c, commitment) {
@@ -90,12 +102,12 @@ func (r *round) membershipProof(c []byte) ([][]byte, error) {
 		}
 	}
 
-	treeRoot, err := t.Root()
+	merkleRoot, err := t.Root()
 	if err != nil {
 		return nil, err
 	}
-	if !bytes.Equal(treeRoot, r.treeRoot) {
-		return nil, fmt.Errorf("incorrect tree root: %v", treeRoot)
+	if !bytes.Equal(merkleRoot, r.merkleRoot) {
+		return nil, fmt.Errorf("incorrect merkleTree root: %v", merkleRoot)
 	}
 
 	proof, err := t.Proof()
