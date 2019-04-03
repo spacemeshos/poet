b8d42ba68be08b60415483a2e3ea0bbfac86a308
Moshe Shababo <dicentralized@gmail.com>
2019-04-03 15:26:47 +0300

    Naming

--- service/round.go
+++ service/round.go
@@ -109,10 +109,10 @@ func (r *round) membershipProof(c []byte, wait bool) ([][]byte, error) {
 		return nil, errors.New("commit not found")
 	}
 
-	var leavesToProves = make(map[uint64]bool)
-	leavesToProves[uint64(ci)] = true
+	var leavesToProve = make(map[uint64]bool)
+	leavesToProve[uint64(ci)] = true
 
-	t := merkle.NewProvingTree(leavesToProves)
+	t := merkle.NewProvingTree(leavesToProve)
 	for _, c := range r.commits {
 		err := t.AddLeaf(c)
 		if err != nil {
