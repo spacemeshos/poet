d8c21874076b7779c0ec92a550a5b857dd359a47
Moshe Shababo <dicentralized@gmail.com>
2019-07-15 19:11:41 +0300

    Process cleanup (#57)

--- integration/harness.go
+++ integration/harness.go
@@ -1,12 +1,16 @@
 package integration
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"github.com/spacemeshos/poet/rpc/api"
 	"google.golang.org/grpc"
+	"net"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"runtime"
 	"time"
 )
 
@@ -24,6 +28,13 @@ func NewHarness(cfg *serverConfig) (*Harness, error) {
 	server, err := newServer(cfg)
 	if err != nil {
 		return nil, err
+	}
+
+	if isListening(cfg.rpcListen) {
+		err = killProcess(cfg.rpcListen)
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	// Spawn a new poet server process.
@@ -86,4 +97,42 @@ func baseDir() (string, error) {
 	baseDir := filepath.Join(os.TempDir(), "poet")
 	err := os.MkdirAll(baseDir, 0755)
 	return baseDir, err
+}
+
+func isListening(addr string) bool {
+	conn, _ := net.DialTimeout("tcp", addr, 1*time.Second)
+	if conn != nil {
+		_ = conn.Close()
+		return true
+	}
+	return false
+}
+
+func killProcess(address string) error {
+	addr, err := net.ResolveTCPAddr("tcp", address)
+	if err != nil {
+		return err
+	}
+
+	var cmd *exec.Cmd
+	if runtime.GOOS == "windows" {
+		args := fmt.Sprintf("(Get-NetTCPConnection -LocalPort %d).OwningProcess -Force", addr.Port)
+		cmd = exec.Command("Stop-Process", "-Id", args)
+	} else {
+		args := fmt.Sprintf("lsof -i tcp:%d | grep LISTEN | awk '{print $2}' | xargs kill -9", addr.Port)
+		cmd = exec.Command("bash", "-c", args)
+	}
+
+	var errb bytes.Buffer
+	cmd.Stderr = &errb
+
+	if err := cmd.Start(); err != nil {
+		return err
+	}
+
+	if err := cmd.Wait(); err != nil {
+		return fmt.Errorf("error during killing process: %s | %s", err, errb.String())
+	}
+
+	return nil
 }
