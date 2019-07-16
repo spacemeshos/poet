9b7764370bd0f964b7d7c4a9f6292ef4d129a822
Noam Nelke <noamnelke@gmail.com>
2019-07-16 09:44:16 +0300

    fix gen_protos.sh

--- rpc/api/gen_protos.sh
+++ rpc/api/gen_protos.sh
@@ -1,22 +1,14 @@
 #!/bin/sh
 
+grpc_gateway_path=$(go list -m -f '{{.Dir}}' github.com/grpc-ecosystem/grpc-gateway)
+googleapis_path="$grpc_gateway_path/third_party/googleapis"
+
+
 # Generate protos.
-protoc -I/usr/local/include -I. \
-       -I$GOPATH/src \
-       -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-       --go_out=plugins=grpc:. \
-       api.proto
+protoc -I. -I$googleapis_path --go_out=plugins=grpc:. api.proto
 
 # Generate web proxy.
-protoc -I/usr/local/include -I. \
-       -I$GOPATH/src \
-       -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-       --grpc-gateway_out=logtostderr=true:. \
-       api.proto
+protoc -I. -I$googleapis_path --grpc-gateway_out=logtostderr=true:. api.proto
 
 # Generate the swagger file which describes the REST API.
-protoc -I/usr/local/include -I. \
-       -I$GOPATH/src \
-       -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-       --swagger_out=logtostderr=true:. \
-       api.proto
+protoc -I. -I$googleapis_path --swagger_out=logtostderr=true:. api.proto
