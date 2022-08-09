0a99269e5520609cb140c17ebc7cca7d4e5e5cf2
Dmitry <yashulyak@gmail.com>
2022-08-09 09:40:18 +0300

    add a todo for code that is not used

--- rpccore/rpcserver.go
+++ rpccore/rpcserver.go
@@ -40,6 +40,7 @@ func NewRPCServer(sig *signal.Signal, datadir string) *RPCServer {
 
 func (r *RPCServer) Compute(ctx context.Context, in *apicore.ComputeRequest) (*apicore.ComputeResponse, error) {
 	challenge := in.D.X
+	// TODO(dshulyak) if this is actually will be used change N to Duration for clarity
 	end := time.Now().Add(time.Duration(in.D.N))
 	securityParam := shared.T
 	_, proof, err := prover.GenerateProofWithoutPersistency(r.datadir, hash.GenLabelHashFunc(challenge), hash.GenMerkleHashFunc(challenge), end, securityParam, prover.LowestMerkleMinMemoryLayer)
