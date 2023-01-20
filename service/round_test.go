090c0c273daec8dd7da266d8f85bf5767365e6f8
Bartosz Różański <bartek.roza@gmail.com>
2023-01-20 13:37:41 +0100

    Save round state when it is created (#230)

--- service/round_test.go
+++ service/round_test.go
@@ -226,6 +226,19 @@ func TestRound_StateRecovery(t *testing.T) {
 		req.True(recovered.isOpen())
 		req.False(recovered.isExecuted())
 	})
+	t.Run("Load state of a freshly opened round", func(t *testing.T) {
+		t.Parallel()
+		// Arrange
+		round, err := newRound(t.TempDir(), 0)
+		t.Cleanup(func() { assert.NoError(t, round.teardown(false)) })
+		req.NoError(err)
+		req.NoError(round.saveState())
+
+		// Act & verify
+		req.NoError(round.loadState())
+		req.True(round.isOpen())
+		req.False(round.isExecuted())
+	})
 	t.Run("Recover executing round", func(t *testing.T) {
 		t.Parallel()
 		tmpdir := t.TempDir()
