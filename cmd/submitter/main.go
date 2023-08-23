de7de71bd6fb91c25253580e7298ab12def19bbb
Bartosz Różański <bartek.roza@gmail.com>
2023-08-23 17:00:19 +0200

    Util to load /Submit

---
+++ cmd/submitter/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"context"
+	"crypto/ed25519"
+	"crypto/rand"
+	"fmt"
+
+	"golang.org/x/sync/errgroup"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	apiv1 "github.com/spacemeshos/poet/release/proto/go/rpc/api/v1"
+	"github.com/spacemeshos/poet/shared"
+)
+
+func submit(client apiv1.PoetServiceClient) error {
+	pubKey, privKey, err := ed25519.GenerateKey(rand.Reader)
+	if err != nil {
+		return fmt.Errorf("failed to generate key: %v", err)
+	}
+	// privKey := ed25519.NewKeyFromSeed(make([]byte, 32))
+	// pubKey := privKey.Public().(ed25519.PublicKey)
+
+	ch := make([]byte, 32)
+	_, _ = rand.Read(ch)
+
+	resp, err := client.PowParams(context.Background(), &apiv1.PowParamsRequest{})
+	if err != nil {
+		return fmt.Errorf("failed to get pow params: %v", err)
+	}
+
+	nonce, err := shared.FindSubmitPowNonce(
+		context.Background(),
+		resp.PowParams.Challenge,
+		ch,
+		pubKey,
+		uint(resp.PowParams.Difficulty),
+	)
+	if err != nil {
+		return fmt.Errorf("failed to find nonce: %v", err)
+	}
+
+	signature := ed25519.Sign(privKey, ch)
+	_, err = client.Submit(context.Background(), &apiv1.SubmitRequest{
+		Nonce:     nonce,
+		Challenge: ch,
+		Pubkey:    pubKey,
+		Signature: signature,
+		PowParams: resp.PowParams,
+	})
+	return err
+}
+
+func main() {
+	conn, err := grpc.Dial("localhost:50002", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	client := apiv1.NewPoetServiceClient(conn)
+
+	var eg errgroup.Group
+	for i := 0; i < 20; i++ {
+		eg.Go(func() error {
+			for j := 0; j < 100; j++ {
+				if err := submit(client); err != nil {
+					return err
+				}
+			}
+			return nil
+		})
+	}
+	err = eg.Wait()
+	if err != nil {
+		panic(err)
+	}
+}
