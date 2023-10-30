553ec5d6eaa23411325ead4ebacac99cbf21d7c8
Bartosz Różański <bartek.roza@gmail.com>
2023-10-30 11:04:29 +0100

    Support certifier

--- registration/config.go
+++ registration/config.go
@@ -1,6 +1,10 @@
 package registration
 
-import "time"
+import (
+	"time"
+
+	"go.uber.org/zap/zapcore"
+)
 
 func DefaultConfig() Config {
 	return Config{
@@ -11,9 +15,24 @@ func DefaultConfig() Config {
 }
 
 type Config struct {
-	PowDifficulty uint `long:"pow-difficulty" description:"PoW difficulty (in the number of leading zero bits)"`
+	// FIXME: remove depreacated PoW
+	PowDifficulty uint `long:"pow-difficulty" description:"(DEPRECATED) PoW difficulty (in the number of leading zero bits)"`
 
 	MaxRoundMembers     int           `long:"max-round-members"     description:"the maximum number of members in a round"`
 	MaxSubmitBatchSize  int           `long:"max-submit-batch-size" description:"The maximum number of challenges to submit in a single batch"`
 	SubmitFlushInterval time.Duration `long:"submit-flush-interval" description:"The interval between flushes of the submit queue"`
+
+	Certifier *CertifierConfig
+}
+
+type CertifierConfig struct {
+	URL    string `long:"certifier-url"    description:"The URL of the certifier service"`
+	PubKey []byte `long:"certifier-pubkey" description:"The public key of the certifier service"`
+}
+
+// implement zap.ObjectMarshaler interface.
+func (c CertifierConfig) MarshalLogObject(enc zapcore.ObjectEncoder) error {
+	enc.AddString("url", c.URL)
+	enc.AddBinary("pubkey", c.PubKey)
+	return nil
 }
--- registration/registration.go
+++ registration/registration.go
@@ -32,7 +32,10 @@ type roundConfig interface {
 	RoundEnd(genesis time.Time, epoch uint) time.Time
 }
 
-var ErrTooLateToRegister = errors.New("too late to register for the desired round")
+var (
+	ErrInvalidCertificate = errors.New("invalid certificate")
+	ErrTooLateToRegister  = errors.New("too late to register for the desired round")
+)
 
 // Registration orchestrates rounds functionality
 // It is responsible for:
@@ -127,6 +130,13 @@ func New(
 		r.powVerifiers = powVerifiers{current: options.powVerifier}
 	}
 
+	if r.cfg.Certifier != nil && r.cfg.Certifier.PubKey != nil {
+		logging.FromContext(ctx).Info("configured certifier", zap.Inline(r.cfg.Certifier))
+	} else {
+		logging.FromContext(ctx).Info("disabled certificate checking")
+		r.cfg.Certifier = nil
+	}
+
 	epoch := r.roundCfg.OpenRoundId(r.genesis, time.Now())
 	round, err := newRound(epoch, r.dbdir, r.newRoundOpts()...)
 	if err != nil {
@@ -136,6 +146,10 @@ func New(
 	r.openRound = round
 
 	return r, nil
+}
+
+func (r *Registration) CertifierInfo() *CertifierConfig {
+	return r.cfg.Certifier
 }
 
 func (r *Registration) Pubkey() ed25519.PublicKey {
@@ -287,18 +301,30 @@ func (r *Registration) newRoundOpts() []newRoundOptionFunc {
 func (r *Registration) Submit(
 	ctx context.Context,
 	challenge, nodeID []byte,
+	// TODO: remove deprecated PoW
 	nonce uint64,
 	powParams PowParams,
+	certificate []byte,
 	deadline time.Time,
 ) (epoch uint, roundEnd time.Time, err error) {
 	logger := logging.FromContext(ctx)
-
-	err = r.powVerifiers.VerifyWithParams(challenge, nodeID, nonce, powParams)
-	if err != nil {
-		logger.Debug("challenge verification failed", zap.Error(err))
-		return 0, time.Time{}, err
+	// Verify if the node is allowed to register.
+	// Support both a certificate and a PoW while
+	// the certificate path is being stabilized.
+	if r.cfg.Certifier != nil && certificate != nil {
+		if !ed25519.Verify(r.cfg.Certifier.PubKey, nodeID, certificate) {
+			return 0, time.Time{}, ErrInvalidCertificate
+		}
+	} else {
+		// FIXME: PoW is deprecated
+		// Remove once certificate path is stabilized and mandatory.
+		err := r.powVerifiers.VerifyWithParams(challenge, nodeID, nonce, powParams)
+		if err != nil {
+			logger.Debug("PoW verification failed", zap.Error(err))
+			return 0, time.Time{}, err
+		}
+		logger.Debug("verified PoW", zap.String("node_id", hex.EncodeToString(nodeID)))
 	}
-	logger.Debug("verified challenge", zap.String("node_id", hex.EncodeToString(nodeID)))
 
 	r.openRoundMutex.RLock()
 	epoch = r.openRound.epoch
