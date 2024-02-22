37f4ae2f1658acbe1db83ed8b016e55293abadd2
Matthias <5011972+fasmat@users.noreply.github.com>
2024-02-22 12:57:51 +0000

    make generate

--- registration/mocks/pow_verifier.go
+++ registration/mocks/pow_verifier.go
@@ -5,6 +5,7 @@
 //
 //	mockgen -typed -package mocks -destination mocks/pow_verifier.go . PowVerifier
 //
+
 // Package mocks is a generated GoMock package.
 package mocks
 
@@ -47,31 +48,31 @@ func (m *MockPowVerifier) Params() registration.PowParams {
 }
 
 // Params indicates an expected call of Params.
-func (mr *MockPowVerifierMockRecorder) Params() *PowVerifierParamsCall {
+func (mr *MockPowVerifierMockRecorder) Params() *MockPowVerifierParamsCall {
 	mr.mock.ctrl.T.Helper()
 	call := mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Params", reflect.TypeOf((*MockPowVerifier)(nil).Params))
-	return &PowVerifierParamsCall{Call: call}
+	return &MockPowVerifierParamsCall{Call: call}
 }
 
-// PowVerifierParamsCall wrap *gomock.Call
-type PowVerifierParamsCall struct {
+// MockPowVerifierParamsCall wrap *gomock.Call
+type MockPowVerifierParamsCall struct {
 	*gomock.Call
 }
 
 // Return rewrite *gomock.Call.Return
-func (c *PowVerifierParamsCall) Return(arg0 registration.PowParams) *PowVerifierParamsCall {
+func (c *MockPowVerifierParamsCall) Return(arg0 registration.PowParams) *MockPowVerifierParamsCall {
 	c.Call = c.Call.Return(arg0)
 	return c
 }
 
 // Do rewrite *gomock.Call.Do
-func (c *PowVerifierParamsCall) Do(f func() registration.PowParams) *PowVerifierParamsCall {
+func (c *MockPowVerifierParamsCall) Do(f func() registration.PowParams) *MockPowVerifierParamsCall {
 	c.Call = c.Call.Do(f)
 	return c
 }
 
 // DoAndReturn rewrite *gomock.Call.DoAndReturn
-func (c *PowVerifierParamsCall) DoAndReturn(f func() registration.PowParams) *PowVerifierParamsCall {
+func (c *MockPowVerifierParamsCall) DoAndReturn(f func() registration.PowParams) *MockPowVerifierParamsCall {
 	c.Call = c.Call.DoAndReturn(f)
 	return c
 }
@@ -83,31 +84,31 @@ func (m *MockPowVerifier) SetParams(arg0 registration.PowParams) {
 }
 
 // SetParams indicates an expected call of SetParams.
-func (mr *MockPowVerifierMockRecorder) SetParams(arg0 any) *PowVerifierSetParamsCall {
+func (mr *MockPowVerifierMockRecorder) SetParams(arg0 any) *MockPowVerifierSetParamsCall {
 	mr.mock.ctrl.T.Helper()
 	call := mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "SetParams", reflect.TypeOf((*MockPowVerifier)(nil).SetParams), arg0)
-	return &PowVerifierSetParamsCall{Call: call}
+	return &MockPowVerifierSetParamsCall{Call: call}
 }
 
-// PowVerifierSetParamsCall wrap *gomock.Call
-type PowVerifierSetParamsCall struct {
+// MockPowVerifierSetParamsCall wrap *gomock.Call
+type MockPowVerifierSetParamsCall struct {
 	*gomock.Call
 }
 
 // Return rewrite *gomock.Call.Return
-func (c *PowVerifierSetParamsCall) Return() *PowVerifierSetParamsCall {
+func (c *MockPowVerifierSetParamsCall) Return() *MockPowVerifierSetParamsCall {
 	c.Call = c.Call.Return()
 	return c
 }
 
 // Do rewrite *gomock.Call.Do
-func (c *PowVerifierSetParamsCall) Do(f func(registration.PowParams)) *PowVerifierSetParamsCall {
+func (c *MockPowVerifierSetParamsCall) Do(f func(registration.PowParams)) *MockPowVerifierSetParamsCall {
 	c.Call = c.Call.Do(f)
 	return c
 }
 
 // DoAndReturn rewrite *gomock.Call.DoAndReturn
-func (c *PowVerifierSetParamsCall) DoAndReturn(f func(registration.PowParams)) *PowVerifierSetParamsCall {
+func (c *MockPowVerifierSetParamsCall) DoAndReturn(f func(registration.PowParams)) *MockPowVerifierSetParamsCall {
 	c.Call = c.Call.DoAndReturn(f)
 	return c
 }
@@ -121,31 +122,31 @@ func (m *MockPowVerifier) Verify(arg0, arg1 []byte, arg2 uint64) error {
 }
 
 // Verify indicates an expected call of Verify.
-func (mr *MockPowVerifierMockRecorder) Verify(arg0, arg1, arg2 any) *PowVerifierVerifyCall {
+func (mr *MockPowVerifierMockRecorder) Verify(arg0, arg1, arg2 any) *MockPowVerifierVerifyCall {
 	mr.mock.ctrl.T.Helper()
 	call := mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Verify", reflect.TypeOf((*MockPowVerifier)(nil).Verify), arg0, arg1, arg2)
-	return &PowVerifierVerifyCall{Call: call}
+	return &MockPowVerifierVerifyCall{Call: call}
 }
 
-// PowVerifierVerifyCall wrap *gomock.Call
-type PowVerifierVerifyCall struct {
+// MockPowVerifierVerifyCall wrap *gomock.Call
+type MockPowVerifierVerifyCall struct {
 	*gomock.Call
 }
 
 // Return rewrite *gomock.Call.Return
-func (c *PowVerifierVerifyCall) Return(arg0 error) *PowVerifierVerifyCall {
+func (c *MockPowVerifierVerifyCall) Return(arg0 error) *MockPowVerifierVerifyCall {
 	c.Call = c.Call.Return(arg0)
 	return c
 }
 
 // Do rewrite *gomock.Call.Do
-func (c *PowVerifierVerifyCall) Do(f func([]byte, []byte, uint64) error) *PowVerifierVerifyCall {
+func (c *MockPowVerifierVerifyCall) Do(f func([]byte, []byte, uint64) error) *MockPowVerifierVerifyCall {
 	c.Call = c.Call.Do(f)
 	return c
 }
 
 // DoAndReturn rewrite *gomock.Call.DoAndReturn
-func (c *PowVerifierVerifyCall) DoAndReturn(f func([]byte, []byte, uint64) error) *PowVerifierVerifyCall {
+func (c *MockPowVerifierVerifyCall) DoAndReturn(f func([]byte, []byte, uint64) error) *MockPowVerifierVerifyCall {
 	c.Call = c.Call.DoAndReturn(f)
 	return c
 }
