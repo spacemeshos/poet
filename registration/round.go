4f1b4c395eb5614fb1de26d8646197315850ef98
Bartosz Różański <bartek.roza@gmail.com>
2024-07-05 09:39:45 +0200

    test canceling pending flush

--- registration/round.go
+++ registration/round.go
@@ -181,14 +181,18 @@ func (r *round) submit(ctx context.Context, key, challenge []byte) (<-chan error
 	}
 
 	if r.batch.Len() >= r.maxBatchSize {
-		r.flushPendingSubmitsLocked()
+		r.flushPendingSubmitsLocked(false)
 	} else if r.batch.Len() == 1 {
 		logging.FromContext(ctx).Debug(
 			"scheduling flush of pending submits",
 			zap.Uint("round", r.epoch),
 			zap.Duration("interval", r.flushInterval),
 		)
-		r.pendingFlush = time.AfterFunc(r.flushInterval, r.flushPendingSubmits)
+		r.pendingFlush = time.AfterFunc(r.flushInterval, func() {
+			r.batchMutex.Lock()
+			defer r.batchMutex.Unlock()
+			r.flushPendingSubmitsLocked(true)
+		})
 	}
 
 	return done, nil
@@ -197,10 +201,14 @@ func (r *round) submit(ctx context.Context, key, challenge []byte) (<-chan error
 func (r *round) flushPendingSubmits() {
 	r.batchMutex.Lock()
 	defer r.batchMutex.Unlock()
-	r.flushPendingSubmitsLocked()
+	r.flushPendingSubmitsLocked(false)
 }
 
-func (r *round) flushPendingSubmitsLocked() {
+func (r *round) flushPendingSubmitsLocked(onTimer bool) {
+	if onTimer && r.pendingFlush == nil {
+		// canceled already
+		return
+	}
 	if r.pendingFlush != nil {
 		r.pendingFlush.Stop()
 		r.pendingFlush = nil
