0cdaf1b717ea45951188e86e13b83c1420e4570c
Matthias Fasching <5011972+fasmat@users.noreply.github.com>
2024-11-06 11:43:25 +0100

    Update merkle-tree library (#535)
    * Update merkle-tree library
    
    * Fix failing test
    
    * Update merkle-tree dependency

--- server/server_test.go
+++ server/server_test.go
@@ -155,7 +155,10 @@ func TestSubmitSignatureVerification(t *testing.T) {
 	req.NoError(err)
 
 	// Submit challenge with invalid signature
-	challenge := []byte("poet challenge")
+	challenge := make([]byte, 32)
+	n, err := rand.Read(challenge)
+	req.NoError(err)
+	req.Equal(32, n)
 	_, err = client.Submit(context.Background(), &api.SubmitRequest{
 		Challenge: challenge,
 		Pubkey:    pubKey,
@@ -213,7 +216,10 @@ func TestSubmitCertificateVerification(t *testing.T) {
 	require.NoError(t, err)
 
 	// Submit challenge with valid signature but invalid pow
-	challenge := []byte("poet challenge")
+	challenge := make([]byte, 32)
+	n, err := rand.Read(challenge)
+	require.NoError(t, err)
+	require.Equal(t, 32, n)
 
 	signature := ed25519.Sign(privKey, challenge)
 
@@ -337,11 +343,16 @@ func TestLoadTrustedKeysAndSubmit(t *testing.T) {
 	trustedKeyCert, err := shared.EncodeCert(&shared.Cert{Pubkey: userPubKey, Expiration: &expiration})
 	require.NoError(t, err)
 
+	challenge := make([]byte, 32)
+	n, err := rand.Read(challenge)
+	require.NoError(t, err)
+	require.Equal(t, 32, n)
+
 	// trusted keys are not loaded
 	_, err = client.Submit(context.Background(), &api.SubmitRequest{
-		Challenge: []byte("challenge"),
+		Challenge: challenge,
 		Pubkey:    userPubKey,
-		Signature: ed25519.Sign(userPrivKey, []byte("challenge")),
+		Signature: ed25519.Sign(userPrivKey, challenge),
 		Certificate: &api.SubmitRequest_Certificate{
 			Data:      trustedKeyCert,
 			Signature: ed25519.Sign(private, trustedKeyCert),
@@ -355,9 +366,9 @@ func TestLoadTrustedKeysAndSubmit(t *testing.T) {
 	require.NoError(t, err)
 
 	_, err = client.Submit(context.Background(), &api.SubmitRequest{
-		Challenge: []byte("challenge"),
+		Challenge: challenge,
 		Pubkey:    userPubKey,
-		Signature: ed25519.Sign(userPrivKey, []byte("challenge")),
+		Signature: ed25519.Sign(userPrivKey, challenge),
 		Certificate: &api.SubmitRequest_Certificate{
 			Data:      trustedKeyCert,
 			Signature: ed25519.Sign(private, trustedKeyCert),
@@ -396,7 +407,10 @@ func TestSubmitAndGetProof(t *testing.T) {
 	// Submit a challenge
 	pubKey, privKey, err := ed25519.GenerateKey(rand.Reader)
 	req.NoError(err)
-	challenge := []byte("poet challenge")
+	challenge := make([]byte, 32)
+	n, err := rand.Read(challenge)
+	req.NoError(err)
+	req.Equal(32, n)
 
 	powParams, err := client.PowParams(context.Background(), &api.PowParamsRequest{})
 	req.NoError(err)
@@ -483,11 +497,19 @@ func TestCannotSubmitMoreThanMaxRoundMembers(t *testing.T) {
 		return err
 	}
 
+	challenge := make([]byte, 32)
+	n, err := rand.Read(challenge)
+	req.NoError(err)
+	req.Equal(32, n)
+
 	// Act
-	req.NoError(submitChallenge([]byte("challenge 1")))
-	req.NoError(submitChallenge([]byte("challenge 2")))
+	challenge[0] = 1
+	req.NoError(submitChallenge(challenge))
+	challenge[0] = 2
+	req.NoError(submitChallenge(challenge))
+	challenge[0] = 3
 	req.ErrorIs(
-		submitChallenge([]byte("challenge 3")),
+		submitChallenge(challenge),
 		status.Error(codes.ResourceExhausted, registration.ErrMaxMembersReached.Error()),
 	)
 	cancel()
@@ -533,13 +555,19 @@ func TestSubmittingChallengeTwice(t *testing.T) {
 		return err
 	}
 
+	challenge := make([]byte, 32)
+	n, err := rand.Read(challenge)
+	req.NoError(err)
+	req.Equal(32, n)
+
 	// Act
-	req.NoError(submitChallenge([]byte("challenge 1")))
+	req.NoError(submitChallenge(challenge))
 	// Submitting the same challenge is OK
-	req.NoError(submitChallenge([]byte("challenge 1")))
-	// Submitting a different challenge with the same nodeID is not OK
+	req.NoError(submitChallenge(challenge))
+	// Submitting a different challenge with the same nodeID is not OK (all bits of the first byte are flipped)
+	challenge[0] = ^challenge[0]
 	req.ErrorIs(
-		submitChallenge([]byte("challenge 2")),
+		submitChallenge(challenge),
 		status.Error(codes.AlreadyExists, registration.ErrConflictingRegistration.Error()),
 	)
 }
@@ -589,10 +617,23 @@ func TestSubmittingWithNeedByTimestamp(t *testing.T) {
 
 	round0End := cfg.Round.RoundEnd(cfg.Genesis.Time(), 0)
 
-	req.NoError(submitChallenge([]byte("at round end"), round0End))
-	req.NoError(submitChallenge([]byte("after round ends"), round0End.Add(time.Minute)))
+	challenge := make([]byte, 32)
+	n, err := rand.Read(challenge)
+	req.NoError(err)
+	req.Equal(32, n)
+
+	// at round end
+	challenge[0] = 1
+	req.NoError(submitChallenge(challenge, round0End))
+
+	// after round ends
+	challenge[0] = 2
+	req.NoError(submitChallenge(challenge, round0End.Add(time.Minute)))
+
+	// before round ends
+	challenge[0] = 3
 	req.ErrorIs(
-		submitChallenge([]byte("before round ends"), round0End.Add(-time.Minute)),
+		submitChallenge(challenge, round0End.Add(-time.Minute)),
 		status.Error(codes.FailedPrecondition, registration.ErrTooLateToRegister.Error()),
 	)
 }
@@ -786,7 +827,10 @@ func TestRegistrationOnlyMode(t *testing.T) {
 	// Submit a challenge
 	pubKey, privKey, err := ed25519.GenerateKey(rand.Reader)
 	req.NoError(err)
-	challenge := []byte("poet challenge")
+	challenge := make([]byte, 32)
+	n, err := rand.Read(challenge)
+	req.NoError(err)
+	req.Equal(32, n)
 
 	powParams, err := client.PowParams(context.Background(), &api.PowParamsRequest{})
 	req.NoError(err)
